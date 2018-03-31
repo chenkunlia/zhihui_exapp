2eb6f6b42e0e000c45a2ebe38ae4d15ff9e6e3ca
axf <2792938834@qq.com>
2018-04-01 02:40:23 +0800

    init

---
+++ siteinfo.js
@@ -0,0 +1,12 @@
+var siteinfo = {
+  'title': '',
+  'uniacid': '2',
+  'acid': '2',
+  'multiid': '0',
+  'version': '1.0',
+  'siteroot': 'https://we7.aixiangfei.cn/app/index.php',
+  'design_method': '3',
+  'redirect_module': '',
+  'template': ''
+}
+module.exports=siteinfo;
